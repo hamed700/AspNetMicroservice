fbd57c5ba94acdb5c486126bed8c01433f67be9d
hamed700 <sh.hoseini68@gmail.com>
2022-04-17 13:11:18 +0430

    Discount Api Done. also Postgresql

---
+++ src/Discount/Discount.API/Program.cs
@@ -0,0 +1,31 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+using Discount.API.Repositories.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+//builder.Services.ConfigurePostgreSql(builder.Configuration, 0);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
