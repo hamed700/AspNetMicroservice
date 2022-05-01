6ec7f2cd9c84d3b61fe2c343211a02b7a3d6bfa5
hamed700 <sh.hoseini68@gmail.com>
2022-05-01 13:48:52 +0430

    Start Grpc - level2

---
+++ src/Discount/Discount.Grpc/Extensions/PostgreSqlMigrate.cs
@@ -0,0 +1,48 @@
+﻿using Npgsql;
+
+namespace Discount.Grpc.Extensions;
+
+public static class PostgreSqlMigrate
+{
+    public static void ConfigurePostgreSql(this IServiceCollection services, IConfiguration configuration, int retry = 0)
+    {
+        int retryAvailabel = retry;
+
+        NpgsqlConnection _connection = new(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        _connection.Open();
+
+        using var command = new NpgsqlCommand
+        {
+            Connection = _connection,
+        };
+
+        try
+        {
+            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+            command.ExecuteNonQuery();
+        }
+        catch (NpgsqlException ex)
+        {
+
+
+            if (retryAvailabel < 50)
+            {
+                retryAvailabel++;
+                System.Threading.Thread.Sleep(2000);
+                ConfigurePostgreSql(services, configuration, retryAvailabel);
+            }
+        }
+    }
+}
--- src/Discount/Discount.Grpc/Program.cs
+++ src/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,13 @@
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Repositories.Interfaces;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+/// <summary>
+/// Add Injects
+/// </summary>
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
@@ -11,7 +18,10 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+/// <summary>
+/// Add Services from Grpc
+/// </summary>
+app.MapGrpcService<DiscountService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
